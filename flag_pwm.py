dd5df1df3c29dc8b68dcbaa0f8c3addadced4ea4
zulie <al2m027109@gmail.com>
2020-09-11 17:05:57 +0800

    可適用於官方韌體 v1.13 版

--- flag_pwm.py
+++ flag_pwm.py
@@ -47,10 +47,9 @@ class PWM():
         high_t = int(period*(self.duty_alt/1023))
         low_t = int(period - high_t)
         
-        
         self.r.loop(False)
-        self.r.write_pulses((high_t,low_t),start=1)
         self.r.loop(True)
+        self.r.write_pulses((high_t,low_t),start=1)
         
     def duty(self,duty):
         self.duty_alt = duty
@@ -63,10 +62,8 @@ class PWM():
             low_t = int(period - high_t)
                     
             self.r.loop(False)
-            self.r.write_pulses((high_t,low_t),start=1)
             self.r.loop(True)
-        
-
+            self.r.write_pulses((high_t,low_t),start=1)
     
     def deinit(self,):  #停止rmt
         channel[self.pwmchannel] = None
@@ -75,16 +72,19 @@ class PWM():
 
 '''
 若頻率設定為 500Hz, 表示 1 秒震動 500 次, 也就是 0.02 秒 1 次, 等於 2000 微秒 1 次 (此處單位為微秒),
-因此週期就是 2000 微秒。若 duty cycle = 512 -> 512/1023 大約等於50 %, 則：
+因此週期就是 2000 微秒。
+若 duty cycle = 512 -> 512/1023 大約等於50 %, 則：
 高電位持續 2000*50% = 1000 微秒, 低電位持續 2000 * 50% = 1000微秒 
 若 duty cycle = 75 % 表示:
 高電位持續 2000*75% = 1500 微秒, 低電位持續 2000 * 25% = 500 微秒
 因此 週期 = 高電位持續時間 + 低電位持續時間
 --------------------------------------------------------------------------------------------------
 若頻率設定為 x, 表示 1 秒震動 x 次, 也就是 1/x 秒 1 次, 等於 1000000 * 1/x 微秒 1 次,
-因此週期就是 1000000 * 1/x 微秒。duty cycle = y -> y/1023 大約等於 y/1023 %, 則:
+因此週期就是 1000000 * 1/x 微秒。
+duty cycle = y -> y/1023 大約等於 y/1023 %, 則:
 高電為持續 1000000 * 1/x * y/1023, 由於 週期 = 高電位持續時間 + 低電位持續時間,
 因此 低電位持續時間 = 週期 - 高電位持續時間
 低電位持續 (1000000 * 1/x) - (1000000 * 1/x * y/1023)
 
 '''
+
